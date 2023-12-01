9f151d99caa99e6990a72b9be790001a709d9cd2
Mani-mozhi <149560008+Mani-mozhi@users.noreply.github.com>
2023-12-01 10:13:42 +0000

    Pending changes exported from your codespace

--- factorial.java
+++ factorial.java
@@ -1,16 +1,13 @@
 import java.util.*;
 class Main{
 public static void main(String[] args){
-System.out.println("PROGRAM TO FIND THE FACTORIAL OF A GIVEN NUMBER
-USING RECURSION");
-System.out.println("ENTER THE NUMBER TO FIND THE FACTORIAL OF A GIVEN
-NUMBER USING RECURSION");
+System.out.println("PROGRAM TO FIND THE FACTORIAL OF A GIVEN NUMBER USING RECURSION");
+System.out.println("ENTER THE NUMBER TO FIND THE FACTORIAL OF A GIVEN NUMBER USING RECURSION");
 Scanner in = new Scanner(System.in);
 int number = in.nextInt();
-System.out.println("FACTORIAL OF A GIVEN NUMBER IS: " +
-factorial(number));
+System.out.println("FACTORIAL OF A GIVEN NUMBER IS: " + factorial(number));
 }
-static int factorial(intnumber){
+static int factorial(int number){
 //factorial program
 if(number<=1)
 {
